d15a80030b6579dd825c3e6fbbd1eb42168e011d
victory377 <victor377228@hotmail.com>
2017-02-05 21:29:35 +0800

    Selection

---
+++ selectionsort.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* insertionSortList(struct ListNode* head) {
+    struct ListNode *ptr,*search,*index;
+    int tmp;
+    ptr = head;
+    
+    while(ptr != NULL){
+        search = ptr;
+        index = search;
+        while(search != NULL){
+            if(search->val < index->val)
+                index = search;
+            search = search->next;
+        }
+        tmp = index->val;
+        index->val = ptr->val;
+        ptr->val = tmp;
+        ptr = ptr->next;
+    }
+    return head;
+}
